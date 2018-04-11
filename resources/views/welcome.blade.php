c2ad0605f6d80c4162b1a3a7a8e01278385e5aa7
luis <luis152288@gmail.com>
2018-04-11 18:37:47 -0400

    accesos y middlewares

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,6 +47,8 @@
 
             .title {
                 font-size: 84px;
+                text-transform: capitalize;
+                
             }
 
             .links > a {
@@ -71,23 +73,19 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Iniciar Sesion</a>
+                        <a href="{{ route('register') }}">Registro</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    RoleS y PermIsos
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Comencemos</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,77 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//routes
+
+Route::middleware(['auth'])->group(function(){
+
+		//roles
+	Route::post('roles/store', 'RoleController@store')->name('roles.store')
+	->middleware('permission:roles.create');
+
+	Route::get('roles', 'RoleController@index')->name('roles.index')
+	->middleware('permission:roles.index');
+
+	Route::get('roles/create', 'RoleController@create')->name('roles.create')
+	->middleware('permission:roles.create');
+
+	Route::put('roles/{role}', 'RoleController@update')->name('roles.update')
+	->middleware('permission:roles.edit');
+
+	Route::get('roles/{role}', 'RoleController@show')->name('roles.show')
+	->middleware('permission:roles.show');
+
+	Route::delete('roles/{role}', 'RoleController@destroy')->name('roles.destroy')
+	->middleware('permission:roles.destroy');
+
+	Route::get('roles/{store}', 'RoleController@edit')->name('roles.edit')
+	->middleware('permission:roles.edit');
+
+
+	//product
+
+	Route::post('products/store', 'ProductController@store')->name('products.store')
+	->middleware('permission:products.create');
+
+	Route::get('products', 'ProductController@index')->name('products.index')
+	->middleware('permission:products.index');
+
+	Route::get('products/create', 'ProductController@create')->name('products.create')
+	->middleware('permission:products.create');
+
+	Route::put('products/{role}', 'ProductController@update')->name('products.update')
+	->middleware('permission:products.edit');
+
+	Route::get('products/{role}', 'ProductController@show')->name('products.show')
+	->middleware('permission:products.show');
+
+	Route::delete('products/{role}', 'ProductController@destroy')->name('products.destroy')
+	->middleware('permission:products.destroy');
+
+	Route::get('products/{store}', 'ProductController@edit')->name('products.edit')
+	->middleware('permission:products.edit');
+
+
+	//users
+
+	Route::get('users', 'UsersController@index')->name('users.index')
+	->middleware('permission:users.index');
+
+	Route::put('users/{role}', 'UsersController@update')->name('users.update')
+	->middleware('permission:users.edit');
+
+	Route::get('users/{role}', 'UsersController@show')->name('users.show')
+	->middleware('permission:users.show');
+
+	Route::delete('users/{role}', 'UsersController@destroy')->name('users.destroy')
+	->middleware('permission:users.destroy');
+
+	Route::get('users/{store}', 'UsersController@edit')->name('users.edit')
+	->middleware('permission:users.edit');
+
+});
